a839b3d46cce1a400b30851bc532983be60e90be
Bassem Ibrahim <bassem.ibrahim@klarna.com>
2025-07-30 17:37:12 +0200

    Do not read index.json if not found

--- packages/core/src/get-stories-from-index.js
+++ packages/core/src/get-stories-from-index.js
@@ -12,11 +12,15 @@ function getStoriesFromIndex(baseUrl) {
   const absoluteUrl = baseUrl.startsWith('file:')
     ? baseUrl.replace('file:', '')
     : baseUrl;
+  const indexJsonPath = path.join(absoluteUrl, 'index.json');
 
-  const indexJsonContent = fs.readFileSync(
-    path.join(absoluteUrl, 'index.json'),
-    'utf8'
-  );
+  // If no index.json file is found, return an empty string.
+  // This is only required for a storybook V7 build.
+  if (!fs.existsSync(indexJsonPath)) {
+    return '';
+  }
+
+  const indexJsonContent = fs.readFileSync(indexJsonPath, 'utf8');
 
   return JSON.stringify(JSON.parse(indexJsonContent));
 }
